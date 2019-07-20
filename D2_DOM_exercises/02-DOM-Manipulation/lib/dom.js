e9ebaf04bb2e92978fe280e1415b38c9d5299177
Shelo <siankhual96@gmail.com>
2019-07-20 20:48:10 +0800

    done ex02

--- D2_DOM_exercises/02-DOM-Manipulation/lib/dom.js
+++ D2_DOM_exercises/02-DOM-Manipulation/lib/dom.js
@@ -21,21 +21,21 @@ module.exports = function runChallenges(check) {
   const borisesEmail = getEmail();
 
   // Ex 2. Change the content of the email input by writing your own email address
-  var document.querySelector("#email").value = "shelmoh@shelomoh.com";
+  document.querySelector("#email").value = "shelomoh@shelomoh.com";
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+  document.querySelector("#email-hint").innerHTML = "<strong>This is my email now</strong>";
 
 
   // Ex 4. Add the .blue CSS class to the th elements
-
-
+  document.querySelectorAll("th").forEach((th)=>{th.classList.add("blue");});
 
   // Ex 5. Count the number of table body rows there are
   //       Make the function teamCount() return it
   const teamCount = () => {
     // TODO: return the number of teams
+    return document.querySelectorAll("tbody tr").length;
   };
 
   // /!\ DO NOT CHANGE THIS LINE - TEST PURPOSES
@@ -43,22 +43,34 @@ module.exports = function runChallenges(check) {
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+  let table = document.querySelector(".table tbody");
+  let row = table.insertRow(-1);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  cell1.innerHTML = "15";
+  cell2.innerHTML = "Recode";
+  cell3.innerHTML = "0";
 
   // Ex 7. Write some code to sum all points given to all teams
   //       Make the function summarizePoints() return it
   const summarizePoints = () => {
     // TODO: return the sum
+    let ttlPoint = 0
+    let allTd = document.querySelectorAll(".table tbody tr td:last-child").forEach((td)=>{
+      ttlPoint += parseInt(td.textContent);
+    });
+    return ttlPoint;
   };
 
 
   // Ex 8. Change the background color of all `<th>` cells to #DDF4FF
+  document.querySelectorAll("th").forEach((th)=>{th.style.backgroundColor = "#DDF4FF";});
 
 
 
   // Ex 9. Remove the "Email:" label from the DOM
-
+  let label = document.querySelector("label[for=\"email\"]").remove();
 
 
 
