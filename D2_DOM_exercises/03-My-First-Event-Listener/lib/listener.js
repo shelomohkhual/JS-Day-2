baf1ad988388b1fabcb2f7e834e5c4d8e1469243
Shelo <siankhual96@gmail.com>
2019-07-21 16:29:14 +0800

    done ex02_03

--- D2_DOM_exercises/03-My-First-Event-Listener/lib/listener.js
+++ D2_DOM_exercises/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
 // TODO: React to a click on the button!
+function myPlay(){
+  document.querySelector("#clickme").innerHTML = "Bingo!";
+  document.querySelector("#clickme").classList.add("disabled");
+  var audio = new Audio("sound.mp3");
+  audio.play();
+}
+document.getElementById("clickme").addEventListener("click", myPlay);
